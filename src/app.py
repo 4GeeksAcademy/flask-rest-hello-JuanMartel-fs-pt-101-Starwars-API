08dbcb1f6388900623a103a58cda8e5ccceec1f7
jjmartel <juanomartel@gmail.com>
2025-07-08 11:31:54 +0000

    endpointsGET

--- src/app.py
+++ src/app.py
@@ -8,15 +8,15 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +27,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -45,7 +50,23 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = People.query.all()
+    results = [p.serialize() for p in people]
+    return jsonify(results), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_person(people_id):
+    person = People.query.get(people_id)
+    if person is None:
+        return jsonify({"msg": "Person not found"}), 404
+    return jsonify(person.serialize()), 200
