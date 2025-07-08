bd93f2fc517fa4dac772d46d4fc39a702493cefa
jjmartel <juanomartel@gmail.com>
2025-07-08 11:11:03 +0000

    models

--- src/models.py
+++ src/models.py
@@ -17,3 +17,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
+    planet_id: Mapped[int] = mapped_column(db.ForeignKey('planet.id'), nullable=True)
+    people_id: Mapped[int] = mapped_column(db.ForeignKey('people.id'), nullable=True)
+
+    user = db.relationship('User', backref='favorites')
+    planet = db.relationship('Planet')
+    people = db.relationship('People')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet": self.planet.serialize() if self.planet else None,
+            "people": self.people.serialize() if self.people else None
+        }
